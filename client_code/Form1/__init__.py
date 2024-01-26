029413734e2777df0f2f7c486a3a36721b1b6ec2
emersonlgregorio@gmail.com <emersonlgregorio@gmail.com>
2024-01-26 01:47:50 +0000

    Edited Form1.RowTemplate1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -6,5 +6,5 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    self.repeating_panel_1.items = anvil.server.call('get_integracoes_erros')
     # Any code you write here will run before the form opens.
