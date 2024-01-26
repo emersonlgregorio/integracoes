299bffcb82c8292f8c8592130c858f30b0db7818
emersonlgregorio@gmail.com <emersonlgregorio@gmail.com>
2024-01-26 12:53:45 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -30,3 +30,15 @@ class Form1(Form1Template):
   def button_all_click(self, **event_args):
     """This method is called when the button is clicked"""
     self.repeating_panel_1.items = anvil.server.call('get_integracoes_all')
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.repeating_panel_1.items = anvil.server.call('get_hoje')
+
+  def text_box_search_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    self.repeating_panel_1.items = anvil.server.call('get_seqPlanilha', self.text_box_search.text)
+
+  def button_search_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.repeating_panel_1.items = anvil.server.call('get_seqPlanilha', self.text_box_search.text)
