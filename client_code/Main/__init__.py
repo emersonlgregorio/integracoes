241ae1fa8ea51a347b5553c4651cd98e7f6860f9
emersonlgregorio@gmail.com <emersonlgregorio@gmail.com>
2024-02-05 13:22:52 +0000

    Edited Main and Monitor

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -16,11 +16,24 @@ class Main(MainTemplate):
     self.image_1.width = '205px'
     
   # Any code you write here will run before the form opens.
+  def switch_to_monitor(self, **event_args):
+    """Switch to the Gallery view."""
+    self.column_panel_body.clear()
+    self.column_panel_body.add_component(open_form('Monitor'))
+    self.headline_made_with_anvil.scroll_into_view()
+    self.deselect_all_links()
+    self.link_gallery.role = 'selected'
 
-  def monitor_link_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('Monitor')
+  def deselect_all_links(self):
+    """Reset all the roles on the navbar links."""
+    for link in self.home_link, self.monitor_link:
+      link.role = ''
+ 
 
-  def esboco_link_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('Esbocos')
+  # def monitor_link_click(self, **event_args):
+  #   """This method is called when the link is clicked"""
+  #   open_form('Monitor')
+
+  # def esboco_link_click(self, **event_args):
+  #   """This method is called when the link is clicked"""
+  #   open_form('Esbocos')
