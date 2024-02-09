36b6f25ec2dd23fef3d9af8afa9d4a044bf08baa
emersonlgregorio@gmail.com <emersonlgregorio@gmail.com>
2024-02-09 19:19:26 +0000

    Edited Main and Conferencia

--- client_code/Conferencia/__init__.py
+++ client_code/Conferencia/__init__.py
@@ -7,10 +7,15 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import datetime
 
 class Conferencia(ConferenciaTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.date_picker_data.format = "%d/%m/%Y"
+    # self.date_picker_data.date = datetime.date.today()
+    if not self.date_picker_data.date and not self.drop_down_deposito.selected_value:
+      self.flow_panel_2.visible = False
 
     # Any code you write here will run before the form opens.
