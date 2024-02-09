63064b6032b3900107d394509d73d5d0088b6189
emersonlgregorio@gmail.com <emersonlgregorio@gmail.com>
2024-02-09 20:39:22 +0000

    Edited Conferencia and Monitor2

--- client_code/Conferencia/__init__.py
+++ client_code/Conferencia/__init__.py
@@ -19,3 +19,19 @@ class Conferencia(ConferenciaTemplate):
       self.flow_panel_2.visible = False
 
     # Any code you write here will run before the form opens.
+  def search(self, **event_args):
+    data = self.date_picker_data.date
+    data = data if data != None else ''
+    deposito = self.drop_down_deposito.selected_value
+    deposito = deposito if deposito != None else ''
+    filtro = f"""
+        where (DATA_MOV_ORIGEM = to_date('{data}','YYYY/MM/DD HH24:MI:SS') 
+          or to_date('{data}','YYYY/MM/DD HH24:MI:SS') is null 
+          or to_date('{data}','YYYY/MM/DD HH24:MI:SS') = '')
+          and
+          (DEPOSITO_ORIGEM = '{deposito}' or '{deposito}' is null or '{deposito}' = '')
+    """
+    print(filtro)
+    self.repeating_panel_1.items = anvil.server.call('movimentacoes', filtro)
+  
+  
