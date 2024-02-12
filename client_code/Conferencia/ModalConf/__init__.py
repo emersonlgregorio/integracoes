f8aa084ce9e4ffb40d2c9b14ee0b79c5fe0e9eb6
emersonlgregorio@gmail.com <emersonlgregorio@gmail.com>
2024-02-12 12:23:11 +0000

    Created Conferencia.ModalConf.RowTemplate7 and Conferencia.ModalConf.RowTemplate8; Edited Conferencia.ModalConf

--- client_code/Conferencia/ModalConf/__init__.py
+++ client_code/Conferencia/ModalConf/__init__.py
@@ -15,6 +15,7 @@ class ModalConf(ModalConfTemplate):
     seqPlanilha = self.item['id_origem']
     self.getItemsSap(seqPlanilha)
     self.getItemIntegracao(seqPlanilha)
+    self.getItemOrigem(seqPlanilha)
 
     # Any code you write here will run before the form opens.
 
@@ -22,9 +23,9 @@ class ModalConf(ModalConfTemplate):
     filtro = f"""
                 AND am."U_RSD_IDUnisystem" = '{seqPlanilha}'
               """
-    print(filtro)
+    # print(filtro)
     dados = anvil.server.call('movimentosSap', filtro)
-    self.headline_sap.text = self.headline_sap.text+f' - Documentos econtratos {len(dados)}'
+    self.headline_sap.text = self.headline_sap.text+f' - Documentos econtrados {len(dados)}'
     self.repeating_panel_sap.items = dados
 
   def getItemIntegracao(self, seqPlanilha, **properties):
@@ -32,5 +33,21 @@ class ModalConf(ModalConfTemplate):
         WHERE SEQ_PLANILHA = '{seqPlanilha}' 
     """
     dados = anvil.server.call('integracoes', filtro)
-    self.headline_3.text = self.headline_3.text + f' - Documentos econtratos {len(dados)}'
+    self.headline_3.text = self.headline_3.text + f' - Documentos econtrados {len(dados)}'
     self.repeating_panel_integracoes.items = dados
+
+  def getItemOrigem(self,seqPlanilha, **properties):
+    filtro = f"""
+            WHERE m.SEQ_PLANILHA = '{seqPlanilha}'
+          """
+    origem = anvil.server.call('dadosOrigem', filtro)
+    origem = origem[0]
+    # print(origem['seq_planilha'])
+    self.text_box_seqPlanilha.text = str(origem['seq_planilha'])
+    self.text_box_nrDocumento.text = origem['nr_documento']
+    self.text_box_tipo.text = origem['modulo']
+    self.date_picker_data.date = origem['data_mvto']
+    self.text_box_item.text = str(origem['item']) + ' ' + origem['descricao_item']
+    self.text_box_qtde.text = origem['qtde']
+    self.text_box_deposito.text = origem['deposito']
+    self.text_box_destino.text = origem['destino']
