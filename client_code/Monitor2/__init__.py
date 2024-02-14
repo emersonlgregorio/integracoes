fd8cce9aa7bc0685adf4da878f0c9c8f0f472149
emersonlgregorio@gmail.com <emersonlgregorio@gmail.com>
2024-02-14 20:10:32 +0000

    Edited Monitor2

--- client_code/Monitor2/__init__.py
+++ client_code/Monitor2/__init__.py
@@ -48,12 +48,12 @@ class Monitor2(Monitor2Template):
     
     if data or rota or status or unidade or seqPlanilha: #Verifica se todos os filtros estão selecionados
       filtro = f"""
-                  where (TO_DATE(TO_CHAR(ai.DATA_CRIACAO,'DD/MM/YYYY'),'DD/MM/YYYY') >= to_date('{data}', 'DD/MM/YYYY')
-                        or to_date('{data}', 'DD/MM/YYYY') is null 
-                        or to_date('{data}', 'DD/MM/YYYY') = '')
-                    and (TO_DATE(TO_CHAR(ai.DATA_CRIACAO,'DD/MM/YYYY'),'DD/MM/YYYY') <= to_date('{data}', 'DD/MM/YYYY')
-                        or to_date('{data}', 'DD/MM/YYYY') is null 
-                        or to_date('{data}', 'DD/MM/YYYY') = '')
+                  where (ai.DATA_CRIACAO >= to_date('{dtInicial}', 'DD-MM-YYYY HH24:MI:SS')
+                        or to_date('{dtInicial}', 'DD-MM-YYYY HH24:MI:SS') is null 
+                        or to_date('{dtInicial}', 'DD-MM-YYYY HH24:MI:SS') = '')
+                    and (ai.DATA_CRIACAO <= to_date('{dtFinal}', 'DD-MM-YYYY HH24:MI:SS')
+                        or to_date('{dtFinal}', 'DD-MM-YYYY HH24:MI:SS') is null 
+                        or to_date('{dtFinal}', 'DD-MM-YYYY HH24:MI:SS') = '')
                     AND (rota = '{rota}' or '{rota}' is null or '{rota}' = '')
                     AND (status = '{status}' or '{status}' is null or '{status}' = '')
                     AND (m.filial = '{unidade}' or '{unidade}' is null or '{unidade}' = '')
