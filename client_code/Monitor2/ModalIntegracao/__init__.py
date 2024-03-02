a7ed7139674d42664c0a60ce963ef0a5b06ebe3a
emersonlgregorio@gmail.com <emersonlgregorio@gmail.com>
2024-03-02 17:28:25 +0000

    Edited Monitor2.ModalIntegracao

--- client_code/Monitor2/ModalIntegracao/__init__.py
+++ client_code/Monitor2/ModalIntegracao/__init__.py
@@ -12,32 +12,40 @@ class ModalIntegracao(ModalIntegracaoTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    
-  
-  # self.preencheOrigem()
-    
-  def preencheOrigem(self, **properties):
-    self.origem = properties['origem'][0]
-    print(self.origem)
-    if self.item['rota'] != 'AplicacoesFarmbox': 
-      self.headline_origem.text = 'Origem: UNISYSTEM'
-      self.date_picker_data_mvto.date = self.origem['data_mvto']
-      self.text_box_deposito.text = self.origem['deposito']
-      self.text_box_destino.text = self.origem['destino']
-      self.text_box_destino.text = self.origem['destino']
-      self.text_box_nr_documento.text = self.origem['nr_documento']
-      self.text_box_item.text = self.origem['item']
-      self.text_box_desc_item.text = self.origem['descricao_produto']
-      self.text_box_quantidade.text = self.origem['quantidade']
-      self.text_box_modulo.text = self.origem['modulo']
+    for i, p in enumerate(properties):
+      if p == 'origem':
+        
+      
+    if self.item['rota'] != 'AplicacoesFarmbox':
+      origem = properties['origem'][0]
+      self.origemUnisystem(origem)
     else:
-      self.origem = properties['origem'][0]
-      self.detalhe = properties['detalhe'][0]
-      print(self.detalhe)
-      self.headline_origem.text = 'Origem: FARMBOX'
-      self.column_panel_unisystem.visible = False
-      self.date_picker_created_at.date = self.origem['created_at']
-      self.date_picker_closed_date.date = self.origem['closed_date']
+      origem = properties['origem'][0]
+      print(origem)
+      print('separa')
+      detalhe = properties['detalhe'][0]
+      print(detalhe)
+      self.origemFarmbox(origem, detalhe)
+
+  
+  def origemUnisystem(self,origem):
+    self.headline_origem.text = 'Origem: UNISYSTEM'
+    self.column_panel_farmbox.visible = False
+    self.date_picker_data_mvto.date = origem['data_mvto']
+    self.text_box_deposito.text = origem['deposito']
+    self.text_box_destino.text = origem['destino']
+    self.text_box_destino.text = origem['destino']
+    self.text_box_nr_documento.text = origem['nr_documento']
+    self.text_box_item.text = origem['item']
+    self.text_box_desc_item.text = origem['descricao_produto']
+    self.text_box_quantidade.text = origem['quantidade']
+    self.text_box_modulo.text = origem['modulo']
+    
+  def origemFarmbox(origem, detalhe):
+    self.headline_origem.text = 'Origem: FARMBOX'
+    self.column_panel_unisystem.visible = False
+    self.date_picker_created_at.date = origem['created_at']
+    self.date_picker_closed_date.date = origem['closed_date']
 
  
 
