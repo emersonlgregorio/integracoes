f865f51e117f14e7e0d056b13d9c33b47f921ff5
emersonlgregorio@gmail.com <emersonlgregorio@gmail.com>
2024-07-12 16:44:40 +0000

    Edited MoviUni

--- server_code/MoviUni.py
+++ server_code/MoviUni.py
@@ -13,42 +13,35 @@ def dadosOrigem(filtro):
   
   query = f"""
             SELECT
-            	T0.SEQ_PLA_NOTA SEQ_PLANILHA,
-            	T0.NR_DOCUMENTO,
-            	T0.FORM_LANCAMENTO MODULO,
-            	T0.DATA_EMISSAO DATA_MVTO
+              T0.SEQ_PLA_NOTA SEQ_PLANILHA,
+              T0.NR_DOCUMENTO,
+              T0.FORM_LANCAMENTO MODULO,
+              T0.DATA_EMISSAO DATA_MVTO,
+              T1.ITEM,
+              T1.DESC_PRODUTO DESCRICAO_PRODUTO,
+              T1.QUANTIDADE,
+              T1.DEPOSITO,
+              T1.DESTINO
             FROM AC_VW_NF_SAIDAS T0
+            INNER JOIN AC_VW_NF_SAI_ITENS T1 ON t0.SEQ_PLA_NOTA = t1.SEQ_PLA_NOTA
             WHERE t0.SEQ_PLA_NOTA = '{filtro}'
             UNION 
             SELECT 
-            	T0.SEQ_PLA_NOTA SEQ_PLANILHA,
-            	T0.NR_DOCUMENTO,
-            	T0.FORM_LANCAMENTO MODULO,
-            	T0.DATA_EMISSAO DATA_MVTO
+              T0.SEQ_PLA_NOTA SEQ_PLANILHA,
+              T0.NR_DOCUMENTO,
+              T0.FORM_LANCAMENTO MODULO,
+              T0.DATA_EMISSAO DATA_MVTO,
+              T1.ITEM,
+              T1.DESC_PRODUTO DESCRICAO_PRODUTO,
+              T1.QUANTIDADE,
+              T1.DEPOSITO,
+              NULL DESTINO
             FROM AC_VW_NF_ENTRADAS T0
+            INNER JOIN AC_VW_NF_ENT_ITENS T1 ON t0.SEQ_PLA_NOTA = t1.SEQ_PLA_NOTA
             WHERE t0.SEQ_PLA_NOTA = '{filtro}'
   """
-  queryDetalhe = f"""
-            SELECT
-            	T1.ITEM,
-            	T1.DESC_PRODUTO DESCRICAO_PRODUTO,
-            	T1.QUANTIDADE,
-            	T1.DEPOSITO,
-            	T1.DESTINO
-            FROM AC_VW_NF_SAI_ITENS T1
-            WHERE t1.SEQ_PLA_NOTA = '{filtro}'
-            UNION 
-            SELECT 
-            	T1.ITEM,
-            	T1.DESC_PRODUTO DESCRICAO_PRODUTO,
-            	T1.QUANTIDADE,
-            	T1.DEPOSITO,
-            	NULL DESTINO
-            FROM AC_VW_NF_ENT_ITENS T1
-            WHERE t1.SEQ_PLA_NOTA = '{filtro}'
-  """
-  origem = anvil.server.call('oracleSelect',query)
-  detalhe = anvil.server.call('oracleSelect',queryDetalhe)
 
-  return origem, detalhe
+  origem = anvil.server.call('oracleSelect',query)
+
+  return origem
 
